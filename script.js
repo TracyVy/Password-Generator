2d8a06f7343f0e20252f6ab9cf3a13d1816ba0a1
Tracy Vy <tracyvy88@gmail.com>
2020-04-18 15:04:28 -0700

    remove no checkbox

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-var yesNew = "";
-var noNew = "";
-var lengthInput = "";
-var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
-var uppeCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var number = '0123456789';
-var symbol = '!@#$%^&*()';
+// var lengthInput = "";
+// var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+// var uppeCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// var number = '0123456789';
+// var symbol = '!@#$%^&*()';
+
+// function askQuestions() {
+//   var text;
+//   if (confirm("Click one)) {
+//     text = "Great, let's begin!";
+// } else {
+//   text = "Okay, I will see you next time you need one.";
+// }
+// };
+
+function yesFunction() {
+  var checkbox = document.getElementById("yesNew");
+  var text = document.getElementById("yesText");
+
+  if (checkbox.checked == true) {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
