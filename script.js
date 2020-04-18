9a97f9d31ea7ade472713bf283a76f70f54359d0
Tracy Vy <tracyvy88@gmail.com>
2020-04-18 14:10:10 -0700

    redoing HTML

--- script.js
+++ script.js
@@ -1,35 +1,7 @@
-var charNum = '>=8, <=128';
+var yesNew = "";
+var noNew = "";
+var lengthInput = "";
 var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 var uppeCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var number = '0123456789';
 var symbol = '!@#$%^&*()';
-
-btnGenerate.addEventListener("click", (e) => {
-  //e.();
-})
-
-lowerBtn.addEventListener("click", (e) => {
-  console.log("Lower Case checked");
-})
-
-upperBtn.addEventListener("click", (e) => {
-  console.log("Upper Case checked");
-})
-
-numBtn.addEventListener("click", (e) => {
-  console.log("Number button checked");
-})
-
-symBtn.addEventListener("click", (e) => {
-  console.log("Special CHaracters checked");
-})
-
-
-var charNum = document.getElementById('charNum');
-var lower = document.getElementById('lowerBtn');
-var upper = document.getElementById('upperBtn');
-var symbol = document.getElementById('symBtn');
-var generate = document.getElementById('generate');
-var password = document.getElementById('password');
-
-
