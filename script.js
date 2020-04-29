d4e9c1d56963c6c90c8f701740e38e852ca8be69
Tracy Vy <tracyvy88@gmail.com>
2020-04-29 14:38:52 -0700

    set empty variables for each if fxn

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var x; // passwordLength
-var newPasswordVal = '';
-var textInputLength = document.getElementById('lengthInput');
+var newPasswordVal = "";
+var textInputLength = document.getElementById("lengthInput");
 
-textInputLength.addEventListener('keyup', validLength);
+textInputLength.addEventListener("keyup", validLength);
 
 // Do you want a new password?
 function yesFunction() {
@@ -11,43 +11,43 @@ function yesFunction() {
 
   if (checkbox.checked == true) {
     //text.style.display = "block";
-    text.style.visibility = 'visible';
+    text.style.visibility = "visible";
   } else {
     //text.style.display = "none";
-    text.style.visibility = 'hidden';
+    text.style.visibility = "hidden";
   }
-};
+}
 
 // Validate password length
 function validLength(e) {
   e.preventDefault();
   if (e.keyCode === 13) {
-    x = document.getElementById('lengthInput').value;
+    x = document.getElementById("lengthInput").value;
 
     if (x < 8 || x > 128) {
       alert("Please enter a number between 8 & 128");
     }
   }
-};
+}
 
 // Random functions
 function randomLower() {
-  const lower = 'abcdefghijklmnopqrstuvwxyz';
+  const lower = "abcdefghijklmnopqrstuvwxyz";
   return lower[Math.floor(Math.random() * lower.length)];
 }
 
 function randomUpper() {
-  const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   return upper[Math.floor(Math.random() * upper.length)];
 }
 
 function randomNum() {
-  const num = '0123456789';
+  const num = "0123456789";
   return num[Math.floor(Math.random() * num.length)];
 }
 
 function randomSym() {
-  const sym = '~!@#$%^&*()-+';
+  const sym = "~!@#$%^&*()-+";
   return sym[Math.floor(Math.random() * sym.length)];
 }
 
@@ -68,27 +68,30 @@ function generateNew() {
   var symCheck = document.getElementById("symBtn");
 
   if (lowerCheck.checked == true) {
+    var newPasswordVal = "";
     newPasswordVal = randomLower();
   }
 
   if (upperCheck.checked == true) {
+    var newPasswordVal = "";
     newPasswordVal += randomUpper();
   }
 
   if (numCheck.checked == true) {
+    var newPasswordVal = "";
     newPasswordVal += randomNum();
   }
 
   if (symCheck.checked == true) {
+    var newPasswordVal = "";
     newPasswordVal += randomSym();
   }
 
   for (var i = newPasswordVal.length; i < x; i++) {
-    newPasswordVal += randomLower();
+    newPasswordVal += randomUpper();
   }
 
-  console.log('newPasswordVal=' + newPasswordVal);
+  console.log("newPasswordVal=" + newPasswordVal);
 
-  document.getElementById('txtPassword').value = newPasswordVal;
-
+  document.getElementById("txtPassword").value = newPasswordVal;
 }
